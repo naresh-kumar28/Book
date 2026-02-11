48af86d31c8ba95ac08310008d91017cc7726465
Naresh Kumar <nareshkumarsah780@gmail.com>
2026-02-11 18:55:25 +0530

    many pages are added

--- website/bookstore/urls.py
+++ website/bookstore/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('about-us/', aboutUs, name='about-us'),
+    path('contact-us/', contactUs, name='contact-us'),
+    path('privacy-policy/',privacyPolicy , name='privacy-policy'),
+    path('terms-condition/',termsCondition , name='terms-condition'),
+    path('return-refund/',returnRefund , name='return-refund'),
 ]
--- website/bookstore/views.py
+++ website/bookstore/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 
 def home(req):
     return render(req, 'home.html')
+
+def aboutUs(req):
+    return render(req, 'pages/about-us.html')
+
+def contactUs(req):
+    return render(req, 'pages/contact-us.html')
+
+def privacyPolicy(req):
+    return render(req, 'pages/privacy-policy.html')
+
+def termsCondition(req):
+    return render(req, 'pages/terms-condition.html')
+
+def returnRefund(req):
+    return render(req, 'pages/return-refund.html')
