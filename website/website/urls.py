9bee5df0012ecde5c8ae5c0b13919ebd6521369c
Naresh Kumar <nareshkumarsah780@gmail.com>
2026-04-10 15:53:31 +0530

    feat: implement core website templates and routing structure for the bookstore application

--- website/website/urls.py
+++ website/website/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('superadmin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    path('manifest.json', TemplateView.as_view(template_name='manifest.json', content_type='application/json')),
+    path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml', content_type='application/xml')),
     path('', include('bookstore.urls')),
 ]
