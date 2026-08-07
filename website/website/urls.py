968dca250a86aef70cbbeac6d715d78dd208a64c
Naresh Kumar <nareshkumarsah780@gmail.com>
2026-08-07 13:13:08 +0530

    feat: implement checkout flow base template, add favicon, and configure project URL structure

--- website/website/urls.py
+++ website/website/urls.py
@@ -18,11 +18,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
 urlpatterns = [
     path('superadmin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     path('manifest.json', TemplateView.as_view(template_name='manifest.json', content_type='application/json')),
     path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml', content_type='application/xml')),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.png', permanent=True)),
     path('', include('bookstore.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
